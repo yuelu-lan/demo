2b8e8fc8de457c095e8b467098fdb053c96ceb52
yuelu <942893986@qq.com>
2024-07-17 13:20:55 +0800

    feat<global>: 修改 tsconfig 使其在 node 环境下能使用 es 模块

--- leet-code/index.ts
+++ leet-code/index.ts
@@ -1,56 +1,35 @@
 /**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
+ * 自顶向下
+ * dp[i][j] = min(dp[i-1][j] + dp[i-1][j-1]) + val[i][j]
  */
+function minimumTotal(triangle: number[][]): number {
+  const dp: number[][] = [];
+  for (let i = 0; i < triangle.length; i++) {
+    dp[i] = [];
+  }
+  dp[0][0] = triangle[0][0];
 
-/**
- * 中序遍历
- */
-function getElementByMid(root: TreeNode | null): number[] {
-  if (!root) return [];
-  return [
-    ...getElementByMid(root.left),
-    root.val,
-    ...getElementByMid(root.right),
-  ];
-}
-
-function getAllElements(
-  root1: TreeNode | null,
-  root2: TreeNode | null,
-): number[] {
-  const leftArr = getElementByMid(root1);
-  const rightArr = getElementByMid(root2);
-
-  let result: number[] = [];
-
-  while (leftArr.length || rightArr.length) {
-    if (!rightArr.length || (leftArr.length && leftArr[0] < rightArr[0])) {
-      result.push(leftArr.shift()!);
-    } else {
-      result.push(rightArr.shift()!);
+  // 在已知 dp[i][j] 的情况下，
+  // 推导 dp[i+1][j]、dp[i+1][j+1] 的值
+  for (let i = 0; i < triangle.length - 1; i++) {
+    for (let j = 0; j <= i; j++) {
+      /**
+       * dp[i + 1][j] 会被计算两次，
+       * 第一次直接赋值（因此使用 Infinity，保证 min 的结果是计算出来的值）
+       * 第二次用本次计算的值与上一次比较，选取最小值
+       * 本次循环计算 1、2 下一次循环计算 2、3
+       * 这两个 2 就需要取最小值
+       */
+      dp[i + 1][j] = Math.min(
+        dp[i + 1][j] ?? Infinity,
+        dp[i][j] + triangle[i + 1][j],
+      );
+      dp[i + 1][j + 1] = Math.min(
+        dp[i + 1][j + 1] ?? Infinity,
+        dp[i][j] + triangle[i + 1][j + 1],
+      );
     }
   }
 
-  return result;
-}
-
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
+  return Math.min(...dp.pop()!);
 }
